aaed9de0dd2366e90a3403178288fd842cd6a4f7
im karthikeyan.v (cyber._.spartan) <33289300+karthi-the-hacker@users.noreply.github.com>
2020-10-25 19:08:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,7 +34,30 @@ sudo apt-get install -y git
 sudo apt-get install -y rename
 sudo apt-get install -y xargs
 sudo apt install -y awscli
-
+sudo apt-get install cargo
+sudo apt install eyewitness
+sudo apt-get install python-requests
+sudo apt-get install gf-complete-tools
+sudo pip install requests
+sudo apt-get install python-dnspython
+sudo apt-get install python-argparse
+sudo pip install dnspython
+sudo pip install argparse
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y jq
+sudo apt-get install -y ruby-full
+sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
+sudo apt-get install -y python-setuptools
+sudo apt-get install -y libldns-dev
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-dnspython
+sudo apt-get install -y git
+sudo apt-get install -y rename
+sudo apt-get install -y xargs
+sudo apt install -y awscli
 
 
 
@@ -51,3 +74,52 @@ echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashacker
 
 go get -u github.com/tomnomnom/httprobe
 go get -u github.com/tomnomnom/assetfinder
+
+go get -u github.com/tomnomnom/httprobe
+go get -u github.com/tomnomnom/assetfinder
+go get -u github.com/tomnomnom/gf
+echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
+cp -r $GOPATH/src/github.com/tomnomnom/gf/examples ~/.gf
+
+cd sources
+git clone https://github.com/maurosoria/dirsearch.git
+git clone https://github.com/Edu4rdSHL/findomain.git
+git clone https://github.com/aboul3la/Sublist3r.git
+git clone https://github.com/projectdiscovery/subfinder.git
+git clone https://github.com/gwen001/github-search.git
+git clone https://github.com/TheRook/subbrute.git
+git clone https://github.com/maurosoria/dirsearch.git
+git clone https://github.com/karthi-the-hacker/P4R4M-HUNT3R.git
+
+
+sudo cp ~/tools/R3C0NH4CK3R/sources/subfinder/cmd/subfinder/subfinder /usr/local/bin/
+
+sudo pip install -r ~/tools/R3C0NH4CK3R/sources/Sublist3r/requirements.txt
+
+sudo pip install -r ~/tools/R3C0NH4CK3R/sources/github-search/requirements.txt
+sudo pip install -r ~/tools/R3C0NH4CK3R/sources/github-search/requirements2.txt
+sudo pip install -r ~/tools/R3C0NH4CK3R/sources/github-search/requirements3.txt
+sudo pip3 install -r ~/tools/R3C0NH4CK3R/sources/github-search/requirements.txt
+sudo pip3 install -r ~/tools/R3C0NH4CK3R/sources/github-search/requirements2.txt
+sudo pip3 install -r ~/tools/R3C0NH4CK3R/sources/github-search/requirements3.txt
+
+cd subfinder/v2/cmd/subfinder
+go build
+mv subfinder /usr/local/bin/
+cd ../../../../
+
+cd findomain
+cargo build --release
+sudo cp target/release/findomain /usr/bin/
+cd ../
+
+cd P4R4M-HUNT3R/
+unzip P4R4M-HUNT3R.zip
+rm P4R4M-HUNT3R.zip
+pip3 install -r ~/tools/R3C0NH4CK3R/sources/P4R4M-HUNT3R/requirements.txt
+pip install -r ~/tools/R3C0NH4CK3R/sources/P4R4M-HUNT3R/requirements.txt
+cp ~/tools/P4R4M-HUNT3R/P4R4M-HUNT3R/gf_profiles/*.json ~/.gf/ 
+source ~/.bashrc
+clear
+
+echo " installation completed 100%"
